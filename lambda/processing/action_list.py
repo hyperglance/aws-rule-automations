a078c34659d3d07feb47669706dda15d6d700c85
Dave Gilmore <dgilmore@hyperglance.com>
2021-02-12 16:48:30 +0000

    Added Docstrings

--- lambda/processing/action_list.py
+++ lambda/processing/action_list.py
@@ -1,7 +1,21 @@
+"""Get Action List
+
+Returns the list of avaiblae actions for use in Hyperglance.
+"""
+
 import os
 import json
 
-def get_action_list():
+def get_action_list() -> list:
+  """ Generates a list of actions for sunsumption by Hyperglance.
+
+  Returns
+  -------
+  list
+    A json formatted list containing the available actions
+
+  """
+
   action_path = '../actions'
   action_lists = os.listdir(action_path)
   action_list = [os.path.splitext(x)[0] for x in action_lists]
@@ -10,4 +24,6 @@ def get_action_list():
   action += json.dumps(action_list)
   action += '}'
 
+  ## TODO: Save to S3 Bucket 
+  
   return action
