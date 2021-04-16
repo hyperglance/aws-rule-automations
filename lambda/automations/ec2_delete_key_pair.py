5987c85cb8477de6021b9a1c9909a5988b2af9df
Dave Gilmore <dgilmore@hyperglance.com>
2021-04-16 16:24:55 +0100

    Updated Key Property

--- lambda/automations/ec2_delete_key_pair.py
+++ lambda/automations/ec2_delete_key_pair.py
@@ -33,7 +33,7 @@ def hyperglance_automation(boto_session, resource_id: str, matched_attributes ='
   """
 
   client = boto_session.client('ec2')
-  keypair = automation_params.get('Key Name')
+  keypair = automation_params['Key Name']
 
   try:
     response = client.delete_key_pair(
