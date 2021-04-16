be6d09dcb911e685d7e1684c1b72ae3855e29b25
Dave Gilmore <dgilmore@hyperglance.com>
2021-04-16 13:41:13 +0100

    Updated path after restructure

--- deployment/terraform/automations/main.tf
+++ deployment/terraform/automations/main.tf
@@ -9,6 +9,6 @@ provider "aws" {
 module "hyperglance_automations" {
   source = "git::https://github.com/hyperglance/terraform-aws-hyperglance.git//modules/hyperglance-automations"
 
-  hyperglance_automation_list    = "../../files/HyperglanceAutomations.json"
-  lambda_package = "../../lambda"
+  hyperglance_automation_list    = "../../../files/HyperglanceAutomations.json"
+  lambda_package = "../../../lambda"
 }
