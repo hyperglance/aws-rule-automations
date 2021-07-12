0c3116111ac0af12fa43873a6460351229040219
David Gill <dgill@hyperglance.com>
2021-07-12 11:52:06 +0100

    Remove underscores to fix InvalidParameterValue

--- lambda/automations/rds_aurora_delete_cluster.py
+++ lambda/automations/rds_aurora_delete_cluster.py
@@ -29,7 +29,7 @@ def hyperglance_automation(boto_session, resource: dict, automation_params = '')
   client.delete_db_cluster(
     DBClusterIdentifier=rds_instance,
     SkipFinalSnapshot=automation_params.get('SkipAuroraSnapshot').lower() in ['true', 'y', 'yes'],
-    FinalDBSnapshotIdentifier='Snapshot_{}'.format(str(uuid.uuid5(uuid.NAMESPACE_DNS, 'hyperglance')))
+    FinalDBSnapshotIdentifier='Snapshot-{}'.format(str(uuid.uuid5(uuid.NAMESPACE_DNS, 'hyperglance')))
   )
 
 
