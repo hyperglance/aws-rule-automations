4a2e545e4d4bbf295de82cb6f117679efe931216
David Gill <dgill@hyperglance.com>
2021-07-13 15:14:09 +0100

    Fix typo

--- lambda/automations/s3_allow_ssl_only.py
+++ lambda/automations/s3_allow_ssl_only.py
@@ -227,7 +227,7 @@ def hyperglance_automation(boto_session, resource: dict, automation_params = '')
   def info() -> dict:
     INFO = {
       "displayName": "S3 Enforce SSL",
-      "description": "Enforces SSL for object accedss",
+      "description": "Enforces SSL for object access",
       "resourceTypes": [
         "S3 Bucket"
       ],
