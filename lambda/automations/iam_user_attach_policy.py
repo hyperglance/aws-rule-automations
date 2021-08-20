00d909004b4f99aa5a9f99057c6502dd638e0029
Jake <joneill@hyperglance.com>
2021-08-20 12:28:00 +0100

    1. Fix iam_user_attach_policy

--- lambda/automations/iam_user_attach_policy.py
+++ lambda/automations/iam_user_attach_policy.py
@@ -6,10 +6,14 @@ by Hyperglance Rule(s)
 This automation will operate across accounts, where the appropriate IAM Role exists.
 
 """
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
-def hyperglance_automation(boto_session, resource: dict, automation_params = ''):
-  """ Attempts to attach an IAM policy to an IAM User
+def hyperglance_automation(boto_session, resource: dict, automation_params=''):
+    """ Attempts to attach an IAM policy to an IAM User
 
   Parameters
   ----------
@@ -21,34 +25,46 @@ def hyperglance_automation(boto_session, resource: dict, automation_params = '')
     Automation parameters passed from the Hyperglance UI
   """
 
-  client = boto_session.client('iam')
+    client = boto_session.client('iam')
+    user_name = resource['attributes']['User Name']
+    policy_name = automation_params.get('Policy')
+    policies = client.list_policies()['Policies']  # no specific API call to get policy by name
 
-  user_name = resource['attributes']['User Name']
-  policy_arn = automation_params.get('Policy')
+    policy_arn = ''
 
+    for policy in policies:
+        if policy_name == policy['PolicyName']:
+            policy_arn = policy['Arn']
+            break
 
-  client.attach_user_policy(
-    UserName=user_name,
-    PolicyArn=policy_arn
-  )
+    if policy_arn == '':
+        logger.error("no such policy: " + policy_name)
+        return
+
+    client.attach_user_policy(
+        UserName=user_name,
+        PolicyArn=policy_arn
+    )
+
 
 def info() -> dict:
-  INFO = {
-    "displayName": "Attach policy to User",
-    "description": "Attaches an existing policy to an IAM User.",
-    "resourceTypes": [
-      "IAM User"
-    ],
-    "params": [
-      {
-        "name": "Policy",
-        "type": "string",
-        "default": ""
-      }
-    ],
-    "permissions": [
-      "iam:AttachUserPolicy"
-    ]
-  }
+    INFO = {
+        "displayName": "Attach policy to User",
+        "description": "Attaches an existing policy to an IAM User.",
+        "resourceTypes": [
+            "IAM User"
+        ],
+        "params": [
+            {
+                "name": "Policy",
+                "type": "string",
+                "default": ""
+            }
+        ],
+        "permissions": [
+            "iam:AttachUserPolicy",
+            "iam:ListPolicies"
+        ]
+    }
 
-  return INFO
+    return INFO
