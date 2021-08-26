60f33205c7e3c149bc226eb463a0cf74a18403a0
Jake <joneill@hyperglance.com>
2021-08-26 10:02:31 +0100

    1. remove assignment to function

--- lambda/automations/s3_clear_bucket.py
+++ lambda/automations/s3_clear_bucket.py
@@ -29,10 +29,12 @@ def hyperglance_automation(boto_session, resource: dict, automation_params = '')
   client = boto_session.client('s3')
   bucket_name = resource['id']
   response = client.list_objects(Bucket=bucket_name)
+  objects = [{'Key': object['Key']} for object in response['Contents']]
+
   response = client.delete_objects(
    Bucket=bucket_name,
    Delete={
-        'Objects':   objects = [{'Key': object['Key']} for object in response['Contents']],
+        'Objects': objects
     }
 )
 
