f69b7eb14439ac1a4a536000580c8010460d909e
Jake O'Neill <joneill@hyperglance.com>
2021-09-02 12:53:46 +0100

    use the contents of the arn as opposed to the uid to determine is_govclodu

--- lambda/processing/automation_routing.py
+++ lambda/processing/automation_routing.py
@@ -13,7 +13,7 @@ time_threshold = 780  # 13 minutes
 def execute_on_resource(automation_to_execute, resource, action_params):
     ## Grab the account and region (some resources don't have a region, default to us-east-1)
     automation_account_id = resource['account']
-    is_govcloud = 'gov-cloud' in resource['uid']
+    is_govcloud = 'aws-us-gov' in resource['arn']
     
     if 'region' in resource and resource['region'] != '': 
         automation_region = resource['region']
