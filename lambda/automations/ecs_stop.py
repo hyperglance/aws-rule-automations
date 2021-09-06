da67a48eea8d2560b1bdc5d17fdd1c4b18450a60
Jake O'Neill <joneill@hyperglance.com>
2021-09-06 12:30:45 +0100

    simplify ecs_stop cluster automation to unconditionally stop all tasks in cluster

--- lambda/automations/ecs_stop.py
+++ lambda/automations/ecs_stop.py
@@ -26,37 +26,19 @@ def hyperglance_automation(boto_session, resource: dict, automation_params=''):
     cluster_arn = resource['arn']
     role_arn = automation_params.get('Role')
 
-    ## Get a list of running tasks for the cluster
+    # Get a list of running tasks for the cluster
     running_tasks = client.list_tasks(
         cluster=cluster_arn,
         desiredStatus='RUNNING'
     )['taskArns']
 
     for task in running_tasks:
-        described_task = client.describe_tasks(
+
+        client.stop_task(
             cluster=cluster_arn,
-            tasks=[task, ]
-        )['running_tasks'][0]
+            task=task
 
-        task_definition = described_task.get('taskDefinitionArn')
-
-        definition = client.describe_task_definition(
-            taskDefinition=task_definition
-        )['taskDefinition']
-
-        if definition.get('executionRoleArn') == role_arn:
-            if 'EC2' in described_task.get('launchType'):
-                client.update_container_instance_state(
-                    cluster=cluster_arn,
-                    containerInstances=[described_task.get('containerInstanceArn')],
-                    status='DRAINING'
-                )
-
-            client.stop_task(
-                cluster=cluster_arn,
-                task=described_task,
-                reason='Privileged tasks are dangerous'
-            )
+        )
 
 
 def info() -> dict:
@@ -67,18 +49,14 @@ def info() -> dict:
             "ECS Cluster"
         ],
         "params": [
-            {
-                "name": "Role",
-                "type": "string",
-                "default": " "
-            }
+
         ],
         "permissions": [
-          "ecs:ListTasks",
-          "ecs:DescribeTasks",
-          "ecs:DescribeTaskDefinition",
-          "ecs:StopTask",
-          "ecs:UpdateContainerInstancesState"
+            "ecs:ListTasks",
+            "ecs:DescribeTasks",
+            "ecs:DescribeTaskDefinition",
+            "ecs:StopTask",
+            "ecs:UpdateContainerInstancesState"
         ]
     }
 
